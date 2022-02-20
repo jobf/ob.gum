8ad3b97497af6752c5e249ae5f867db51d76b4be
jf <j@b.f>
2022-02-20 02:54:20 +0000

    start adding rudimentary mouse support

--- src/ob/gum/Core.hx
+++ src/ob/gum/Core.hx
@@ -17,10 +17,12 @@ class Gum {
 	public var config(default, null):GumConfig;
 	var tickDurationMs:Int;
 	var isUpdating:Bool;
+	var mouse:MouseHandler;
 
 	public function new(window:Window, ?config:GumConfig) {
 		isUpdating = false;
 		this.window = window;
+		this.mouse = new MouseHandler();
 		this.config = config != null ? config : {
 			framesPerSecond: 60,
 			drawOnlyWhenRequested: false
@@ -46,6 +48,7 @@ class Gum {
 	public function onUpdate(deltaTime:Int):Void {
 		if (!isUpdating)
 			return;
+		mouse.update();
 		elapsedMsSinceLastTick += deltaTime;
 		if (elapsedMsSinceLastTick >= tickDurationMs) {
 			elapsedMsSinceLastTick = 0;
@@ -68,18 +71,22 @@ class Gum {
 	}
 
 	public function onMouseMove(x:Float, y:Float):Void {
+		mouse.onMouseMove(x, y);
 		loop.onMouseMove(x, y);
 	}
 
 	public function onMouseDown(x:Float, y:Float, button:MouseButton):Void {
+		mouse.onMouseDown(x, y, button);
 		loop.onMouseDown(x, y, button);
 	}
 
 	public function onMouseUp(x:Float, y:Float, button:MouseButton):Void {
+		mouse.onMouseUp(x, y, button);
 		loop.onMouseUp(x, y, button);
 	}
 
 	public function onMouseScroll(deltaX:Float, deltaY:Float, wheelMode:MouseWheelMode):Void {
+		mouse.onMouseScroll(deltaX, deltaY, wheelMode);
 		loop.onMouseScroll(deltaX, deltaY, wheelMode);
 	}
 
@@ -143,4 +150,136 @@ class LoadingLoop implements ILoop {
 	public function onMouseScroll(deltaX:Float, deltaY:Float, wheelMode:MouseWheelMode) {}
 
 	public function onWindowResize(width:Int, height:Int) {}
+}
+
+typedef Rectangle = {
+	x:Int,
+	y:Int,
+	wide:Int,
+	high:Int
+}
+
+/*
+	public var pixelX(default, null):Int;
+	public var pixelY(default, null):Int;
+	public var pixelWidth(default, null):Int;
+	public var pixelHeight(default, null):Int;
+*/
+
+class Interactive<T> {
+	public function new(target:T, isOverlapping:Void->Bool, onMouseDown:T->Void=null, ?onMouseUp:T->Void=null, ?onMouseOver:T->Void=null, ?onMouseOut:T->Void=null){
+		this.target = target;
+		isOverlapping = overlapsMouse;
+		this.onMouseDown = onMouseDown;
+		this.onMouseUp = onMouseUp;
+		this.onMouseOver = onMouseOver;
+		this.onMouseOut = onMouseOut;
+		pixelX = bounds.x;
+		pixelY = bounds.y;
+		pixelWidth = bounds.wide;
+		pixelHeight = bounds.high;
+	}
+
+	var target:T;
+	var isOverlapping:(x:Float, y:Float)->Bool;
+	var onMouseDown:T->Void;
+	var onMouseUp:T->Void;
+	var onMouseOver:T->Void;
+	var onMouseOut:T->Void;
+
+	public function overlapsMouse(x:Float, y:Float) {
+		return isOverlapping(x, y);
+	}
+
+	public function mouseOver(x:Float, y:Float) {
+		if onMouseOver == null return;
+		onMouseOver(x, y);
+	}
+
+	public function mouseDown(x:Float, y:Float, button:MouseButton) {
+		if onMouseDown == null return;
+		onMouseDown(x, y, button);
+	}
+
+	public function mouseUp(x:Float, y:Float, button:MouseButton) {
+		if onMouseUp == null return;
+		onMouseUp(x, y);
+	}
+
+	public function mouseScroll(deltaX:Float, deltaY:Float, wheelMode:MouseWheelMode) {
+		if onMouseScroll == null return;
+		mouseOver(deltaX, deltaY, wheelMode);
+	}
+
+	public function mouseScroll(deltaX:Float, deltaY:Float, wheelMode:MouseWheelMode) {
+		if onMouseScroll == null return;
+		mouseOver(deltaX, deltaY, wheelMode);
+	}
+}
+
+class MouseHandler{
+	var targets:Array<Interactive<Any>> = [];
+	var mouseOver:Array<Interactive<Any>> = [];
+	var mouseDown:Array<Interactive<Any>> = [];
+	var mouseClicked:Array<Interactive<Any>> = [];
+	var mouseX:Float = 0;
+	var mouseY:Float = 0;
+	var justMoved:Bool = false;
+	var justReleased:Bool = false;
+	var isMouseDown:Bool = false;
+	var isMouseUp:Bool = false;
+
+	public function new(){
+		
+	}
+	
+	public function add<Any>(target:Interactive<Any>):Void{
+		targets.push(target);
+	}
+	
+	inline function overlaps(x:Float, y:Float, target:Interactive<Any>){
+		var overlapsX = x >= target.x && x <= x + target.pixelWidth;
+		var overlapsY = y >= target.y && y <= y + target.pixelHeight;
+		return  overlapsX && overlapsY;
+	}
+
+	function getTargetsUnderMouse():Array<Interactive<Any>>{
+		return targets.filter(interactive -> interactive.overlapsMouse())
+	}
+
+	public function update() {
+		var targetsUnderMouse getTargetsUnderMouse(mouseX, mouseY);
+		for(t in targetsUnderMouse){
+			t.mouseOver(mouseX, mouseY);
+			if(justMoved){
+				t.mouseMove(mouseX, mouseY);
+			}
+			if(isMouseDown){
+				t.mouseDown(mouseX, mouseY);
+			}
+		}
+		
+		justMoved = false;
+		// justReleased = false; todo ! handle each mouse button
+
+	}
+	public function onMouseMove(x:Float, y:Float) {
+		mouseX = x;
+		mouseY = y;
+		justMoved = true;
+	}
+
+	public function onMouseDown(x:Float, y:Float, button:MouseButton) {
+		isMouseDown = true;
+		isMouseUp = false;
+	}
+
+	public function onMouseUp(x:Float, y:Float, button:MouseButton) {
+		justReleased = true;
+		isMouseDown = false;
+		isMouseUp = true;
+	}
+
+	public function onMouseScroll(deltaX:Float, deltaY:Float, wheelMode:MouseWheelMode) {}
+
 }
